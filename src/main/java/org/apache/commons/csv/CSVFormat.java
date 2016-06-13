780891564251f0d0f4381a85cafb74f9a0925ba6
grigorasmariusviorel <grigoras.marius.viorel@gmail.com>
2016-06-13 15:51:03 +0300

    Update CSVFormat.java

--- src/main/java/org/apache/commons/csv/CSVFormat.java
+++ src/main/java/org/apache/commons/csv/CSVFormat.java
@@ -794,7 +794,7 @@ public final class CSVFormat implements Serializable {
      */
     private void validate() throws IllegalArgumentException {
         for (int i=0; i<2; i++) {
-            System.out.print('.');
+            System.out.print('*');
             try {
                 Thread.currentThread().sleep(1000);
             } catch (InterruptedException e) {
